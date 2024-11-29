b20984c067eb8c11d16c9b76e2b1c83e5fd01487
snyatass <virtualinfinitylab@gmail.com>
2024-11-29 15:20:51 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    // Абстрактний продукт A
+    public interface IChair
+    {
+        string GetModel();
+    }
+
+    // Абстрактний продукт B
+    public interface ITable
+    {
+        string GetMaterial();
+    }
+
+    // Абстрактна фабрика
+    public interface IFurnitureFactory
+    {
+        IChair CreateChair();
+        ITable CreateTable();
+    }
+
+    // Реалізація продуктів для Стильної фабрики
+    public class ModernChair : IChair
+    {
+        public string GetModel() => "Модерний стілець";
+    }
+
+    public class ModernTable : ITable
+    {
+        public string GetMaterial() => "Скляний стіл";
+    }
+
+    // Реалізація продуктів для Класичної фабрики
+    public class ClassicChair : IChair
+    {
+        public string GetModel() => "Класичний стілець";
+    }
+
+    public class ClassicTable : ITable
+    {
+        public string GetMaterial() => "Дерев'яний стіл";
+    }
+
+    // Конкретна фабрика - Modern
+    public class ModernFurnitureFactory : IFurnitureFactory
+    {
+        public IChair CreateChair() => new ModernChair();
+        public ITable CreateTable() => new ModernTable();
+    }
+
+    // Конкретна фабрика - Classic
+    public class ClassicFurnitureFactory : IFurnitureFactory
+    {
+        public IChair CreateChair() => new ClassicChair();
+        public ITable CreateTable() => new ClassicTable();
+    }
+}
